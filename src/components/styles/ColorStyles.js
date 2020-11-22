4f1625a212611037724e8ec9b0222731eb2d1d8c
abusiraj84 <abusiraj84@gmail.com>
2020-11-22 13:37:43 +0300

    1

--- src/components/styles/ColorStyles.js
+++ src/components/styles/ColorStyles.js
@@ -4,7 +4,7 @@ export const themes = {
     text2: `rgba(0,0,0,0.7)`,
     primary: `#00CFFD`,
     secondary: "#070707",
-    backgroundColor: `#234461`,
+    backgroundColor: `rgb(31, 31, 71)`,
     card: {
       backgroundColor: `rgba(255, 255, 255, 0.6)`,
       boxShadow: `0px 50px 100px rgba(34, 79, 169, 0.3),
