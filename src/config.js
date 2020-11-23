0d32ab60edd202f0bac07b56e37627675713efd2
abusiraj84 <abusiraj84@gmail.com>
2020-11-24 01:38:03 +0300

    1

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
   // siteUrl: "http://localhost:3000/",
-  siteUrl: "http://68.183.216.171/Devam-Api/public/api",
+  siteUrl: "https://devam.website/Devam-Api/public/api",
 };
