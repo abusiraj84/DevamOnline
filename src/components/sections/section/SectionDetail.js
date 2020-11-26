efe978222a612b8657fb770faeea1d080cfa53e6
abusiraj84 <abusiraj84@gmail.com>
2020-11-26 06:32:14 +0300

    1

--- src/components/sections/section/SectionDetail.js
+++ src/components/sections/section/SectionDetail.js
@@ -101,19 +101,9 @@ function SectionDetail(props) {
   return (
     <Wrapper>
       <BoxImgWrapper imgcolor={imgcolor}>
-        <BoxImg
-          src={
-            `https://devam.website/admin/_lib/file/img/${img}` ||
-            "images/ills/example1.svg"
-          }
-        />
+        <BoxImg src={img || "images/ills/example1.svg"} />
       </BoxImgWrapper>
-      <Logo
-        src={
-          `https://devam.website/admin/_lib/file/img/${logo}` ||
-          "images/icons/cubase.svg"
-        }
-      />
+      <Logo src={logo || "images/icons/cubase.svg"} />
       <Title>{title}</Title>
       <Desc1>
         {sections || "0"} أقسام - {hours || "2"} ساعة
